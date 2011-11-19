012a6a54f794eb37e3a545a49098e65193265da5
David Snyder <dsnyder90@gmail.com>
2011-11-19 00:53:00 -0600

    deduped some of the test defs

--- test/quadkeys/test/core.clj
+++ test/quadkeys/test/core.clj
@@ -5,19 +5,15 @@
 (deftest pixels-to-tile-256
   (is (= (pixels-to-tile 256 256) [1 1])))
 
-(deftest lat-lng-to-pixels-convert-5
-  (is (= (lat-lng-to-pixels 30.297463 -97.757506 5) [1871,3372])))
-
-(deftest lat-lng-to-pixels-convert-11
+(deftest lat-lng-to-pixels-conversions
+  (is (= (lat-lng-to-pixels 30.297463 -97.757506 5) [1871,3372]))
   (is (= (lat-lng-to-pixels 30.297463 -97.757506 11) [119774, 215807])))
 
 (deftest quad-for-lvl-3
   (is (= 2 (quad-for-lvl 3 5 3))))
 
-(deftest tile-to-quadkey-3
-  (is (= "213" (tile-to-quadkey 3 5 3))))
-
-(deftest tile-to-quadkey-11
+(deftest tile-to-quadkey-conversions
+  (is (= "213" (tile-to-quadkey 3 5 3)))
   (is (= "03133233332" (tile-to-quadkey 119774, 215807,11))))
 
 (deftest lat-lng-to-quadkey-11
